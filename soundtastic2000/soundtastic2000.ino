df7b0d7774296031d0cfe0ad6dc3459e5c763a48
M4L3D1V3 <32615956+M4L3D1V3@users.noreply.github.com>
2021-03-15 22:22:36 +0100

    Update soundtastic2000.ino

--- soundtastic2000/soundtastic2000.ino
+++ soundtastic2000/soundtastic2000.ino
@@ -7,58 +7,20 @@
 #include "DFRobotDFPlayerMini.h"
 #include "Arduino.h"
 #include "SoftwareSerial.h"
-#include <EEPROM.h>
+
+#define VOLUME 30
 
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer; 
 
-// This retains the volume persistently.
-int eeprom_addr = 0;  // EEPROM address
-int volume = EEPROM.read(eeprom_addr);  // init read of volume
+const int LED  = 13;   // pinout for debug led
 
-const byte interruptPin2 = 2; // LOW
-const byte interruptPin3 = 3; // HIGH
-
-int led  = 13;   // pinout for debug led
-
-// makes volume a bit quiter
-int quiter() {
-  if (volume > 0) {
-    volume = volume - 5;
-    EEPROM.write(eeprom_addr, volume);  // writes the value in the EEPROM
-    return volume;
-  }
-}
-
-// makes volume a bit louter
-int louder() {
-  if (volume < 30) {
-    volume = volume + 5;
-    EEPROM.write(eeprom_addr, volume);  // writes the value in the EEPROM
-    return volume;
-  }
-}
-
-// the interrupt for quiter()
-void interruptLow() {
-  myDFPlayer.volume(quiter());
-  Serial.println((volume));
-}
-
-// the interrupt for louder()
-void interruptHigh() {
-  myDFPlayer.volume(louder());
-  Serial.println((volume));
-}
 
 // setup sequence
 void setup() {
-  // interrupt pins
-  pinMode(interruptPin2, INPUT_PULLUP); 
-  pinMode(interruptPin3, INPUT_PULLUP);
   
   // debug led pinout
-  pinMode(led, OUTPUT);            
+  pinMode(LED, OUTPUT);            
 
   // console outputs
   mySoftwareSerial.begin(9600);
@@ -69,27 +31,31 @@ void setup() {
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
     Serial.println(F("2.Please insert the SD card!"));
-    digitalWrite(led, HIGH); // if not ready the led will be shine :)
+    digitalWrite(LED, HIGH); // if not ready the led will be shine :)
     while(true);
   }
 
+  // equalizer
+  myDFPlayer.EQ(DFPLAYER_EQ_CLASSIC);
+
   // if card reader is ready the led will not shine :(
-  digitalWrite(led, LOW);
+  digitalWrite(LED, LOW);
   Serial.println(F("EVERYTHING IS OKAY!"));
   myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
   myDFPlayer.setTimeOut(500); //Set serial communictaion time out 500ms
-  myDFPlayer.volume(volume);
+  myDFPlayer.volume(VOLUME);
   myDFPlayer.play(1);  //Play the first mp3
   delay(1000);
   myDFPlayer.enableLoopAll();  
   delay(1000);
 
-  // attaches the interrupts
-  attachInterrupt(digitalPinToInterrupt(interruptPin2), interruptLow, RISING);
-  attachInterrupt(digitalPinToInterrupt(interruptPin3), interruptHigh, RISING);
   
 }
 
 // not used here
 void loop() {
+  while(true){
+    Serial.println(F("RUNNING"));
+    delay(1000);
+  } 
 }
